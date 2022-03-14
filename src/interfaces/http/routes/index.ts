eafdfee581a9b181feb45403bf1d4b685fd1a8df
Douyemiene <viktormiene.vm@gmail.com>
2022-03-14 14:21:34 +0100

    feat: add /get for 3scale

--- src/interfaces/http/routes/index.ts
+++ src/interfaces/http/routes/index.ts
@@ -5,6 +5,9 @@ import container from "../../../di-setup";
 
 const app = express();
 app.use(express.json());
+app.get('/', (req,res)=> {
+  res.json({message: 'Make your orders with this API'})
+})
 app.use("/orders", OrderRouter);
 
 const { messenger } = container.cradle;
