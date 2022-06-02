2cd9b10635b219b609624887a4f671439218af3a
Douyemiene <viktormiene.vm@gmail.com>
2022-06-02 07:26:08 +0100

    chore: create order refactor

--- src/infra/messaging/messenger.ts
+++ src/infra/messaging/messenger.ts
@@ -1,25 +1,24 @@
-import amqp, { Channel, Connection, Message } from "amqplib";
+import amqp, { Channel, Connection } from "amqplib";
 import { Status } from "../../domain/order";
 import OrderUseCase from "../../usecases/OrderUseCase";
 
 export interface IMessenger {
+  channel: Channel
   createChannel(): Promise<void>;
   assertExchange(exchange: string, exchangeType: string)
   publishToExchange(exchange: string, key: string, msg: Object)
   sendToQueue(queue: string, content: Object): void;
   assertQueue(queue: string): Promise<void>;
-  consume(): Promise<void>
-  // consumePaymentSuccess(): Promise<void>;
-  // consumePaymentFailure(): Promise<void>;
+  //consume(): Promise<void>
+ 
   
 }
 
 export class Messenger implements IMessenger {
-  private channel!: Channel;
-  private orderUseCase: OrderUseCase;
+  channel: Channel;
 
-  constructor({ orderUseCase }: { orderUseCase: OrderUseCase }) {
-    this.orderUseCase = orderUseCase;
+  constructor() {
+  
   }
 
   async createChannel(): Promise<void> {
@@ -31,7 +30,7 @@ export class Messenger implements IMessenger {
     await this.assertExchange('paymentEvents','topic')
     await this.assertQueue("orders");
     await this.bindQueue('orders', 'paymentEvents', 'payments.status.*');
-    await this.consume()
+    //await this.consume()
   }
 
   
@@ -60,62 +59,6 @@ export class Messenger implements IMessenger {
     this.channel.sendToQueue(queue, Buffer.from(JSON.stringify(content)));
   }
 
-  async consume(): Promise<void> {
-    this.channel.consume(
-      "orders",
-      async (msg: Message | null) => {
-        if (msg) {
-          const routingKey = msg.fields.routingKey
-          const data = JSON.parse(msg.content.toString());
-          //console.log(`msg - ${routingKey}`)
-
-          if(routingKey == 'payments.status.success'){  
-              //console.log('payments.status.success')
-              //update order status and publish order completed event
-              await this.orderUseCase.findByIdAndUpdateStatus(
-                data.ref,
-                Status.SUCCESS
-              );
-
-              const order = await this.orderUseCase.getOrderById(data.ref);
-              this.publishToExchange('orderEvents', 'orders.status.completed', {order})
-          }
-          else if(routingKey == 'payments.status.failed'){
-            //console.log('payments.status.failed')
-                //update order status and publish order completed event
-                await this.orderUseCase.findByIdAndUpdateStatus(
-                  data.ref,
-                  Status.FAILURE
-                );
-                const order = await this.orderUseCase.getOrderById(data.ref);
-                this.publishToExchange('orderEvents', 'orders.status.failed', {order})
-          }
-         
-        }
-      },
-      { noAck: true }
-    );
-  }
-
-  // async consumePaymentFailure() {
-  //   this.channel.consume(
-  //     "payment_failure",
-  //     async (msg: Message | null) => {
-  //       if (msg) {
-
-  //         const data = JSON.parse(msg.content.toString());
-  //         await this.orderUseCase.findByIdAndUpdateStatus(
-  //           data.ref,
-  //           Status.FAILURE
-  //         );
-        
-  //         const order = await this.orderUseCase.getOrderById(data.ref);
-  //         this.publishToExchange('orderEvents', 'orders.status.failed', {order})
-  //       }
-  //     },
-  //     { noAck: true }
-  //   );
-  // }
 }
 
 export default Messenger;
